a711a197fc9079c8aa1bb4096f1eadcd773e02a7
bobbyduhbrain <Mike@d4-50-160-48.try.wideopenwest.com>
2014-06-29 23:17:56 -0400

    Created a devicesController

--- config/routes.go
+++ config/routes.go
@@ -2,11 +2,11 @@ package config
 
 import(
   "net/http"
-  "github.com/bobbyduhbrain/go-play/lib/airplay"
+  "github.com/bobbyduhbrain/go-play/lib/airplay/devices"
 )
 
 type Routes struct{}
 
 func (config *Routes) Draw(){
-  http.HandleFunc("/devices", new(airplay.Browser).List)
+  http.HandleFunc("/devices", new(airplay.DevicesController).Index)
 }
