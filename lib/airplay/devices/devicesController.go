ded4b65fe1865391545d02f2ed64a74cc8387a01
bobbyduhbrain <Mike@d4-50-160-48.try.wideopenwest.com>
2014-06-29 23:38:24 -0400

    Removed old Routes code.

--- lib/airplay/devices/devicesController.go
+++ lib/airplay/devices/devicesController.go
@@ -11,13 +11,6 @@ import(
 //
 type DevicesController struct{}
 
-//
-//  Draw Routes for DevicesController actions
-//
-func (airplay *DevicesController) Routes(){
-  http.HandleFunc("/devices", new(DevicesController).Index)
-}
-
 
 //
 //  Lists the Airplay compatible devices on the local area network
