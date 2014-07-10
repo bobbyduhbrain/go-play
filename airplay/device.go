9e8ae665180cd61a2990aa596d366600b8ee5873
bobbyduhbrain <mkolich.cubekaraoke@gmail.com>
2014-07-10 19:10:50 -0400

    Somehow device.go didn't get committed.

---
+++ airplay/device.go
@@ -0,0 +1,13 @@
+package airplay
+
+type Device struct{
+  Host string
+  Port int
+  ServerInfo map[string]string
+}
+
+func (d *Device) Update(host string, port int, si map[string]string){
+  d.Host = host
+  d.Port = port
+  d.ServerInfo = si
+}
