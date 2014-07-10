72cd7363c1865e1fb8fc5228cdcd8574973b9046
bobbyduhbrain <mkolich.cubekaraoke@gmail.com>
2014-07-10 17:28:55 -0400

    Added more info to the Device struct.

--- airplay/devices/device.go
+++ airplay/devices/device.go
@@ -1,9 +1,13 @@
 package devices
 
 type Device struct{
-  Data map[string]string
+  Host string
+  Port int
+  ServerInfo map[string]string
 }
 
-func (d *Device) Update(data map[string]string){
-  d.Data = data
+func (d *Device) Update(host string, port int, si map[string]string){
+  d.Host = host
+  d.Port = port
+  d.ServerInfo = si
 }
--- airplay/service/browser.go
+++ airplay/service/browser.go
@@ -45,22 +45,22 @@ func (b *Browser) browse(op *dnssd.BrowseOp, err error, device_servicable bool, 
 //
 //  DNSSD callback function for resolving service discovery
 //
-func (b *Browser) resolve_service_discovery(op *dnssd.ResolveOp, err error, host string, port int, metadata map[string]string){
+func (b *Browser) resolve_service_discovery(op *dnssd.ResolveOp, err error, host string, port int, txt map[string]string){
   if err != nil {
     log.Printf("Resolving discovery failed: %s", err)
     return
   }
-  update_device_map(b.DeviceMap, op.Name(), metadata)
+  update_device_map(b.DeviceMap, op.Name(), host, port, txt)
 }
 
 //
 //  Takes the DeviceMap of a Browser and updates the key-value with newly discovered information
 //
-func update_device_map(dm map[string]*devices.Device, device_name string, device_metadata map[string]string){
-  if dev,mapped := dm[device_name]; !mapped {
-    dm[device_name] = &devices.Device { Data: device_metadata}
+func update_device_map(dm map[string]*devices.Device, name string, host string, port int, txt map[string]string){
+  if dev,mapped := dm[name]; !mapped {
+    dm[name] = &devices.Device { Host: host, Port: port, ServerInfo: txt}
   } else {
-    dev.Update(device_metadata)
-    log.Printf("%s: %v", device_name, dev.Data["features"])
+    dev.Update(host, port, txt)
+    log.Printf("%v", *dev)
   }
 }
