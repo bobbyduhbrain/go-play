95690e30aa99ab5f3833fea899adbec863ff14e9
bobbyduhbrain <mkolich.cubekaraoke@gmail.com>
2014-07-10 19:24:27 -0400

    Removed unneeded port flag.

--- airplay.go
+++ airplay.go
@@ -2,17 +2,8 @@ package main
 
 import (
   "github.com/bobbyduhbrain/go-play/airplay"
-  
-  "flag"
   "time"
 )
-
-var port int
-
-func init() {
-  flag.IntVar(&port, "port", 8080, "port to run the server on")
-  flag.Parse()
-}
 
 func main(){
   browser := airplay.NewBrowser()
